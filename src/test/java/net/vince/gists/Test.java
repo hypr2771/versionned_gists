331c2c562874ee3dd32c115c0b1998b0b1a58849
castelluciv <Vincent Castelluci>
2019-03-31 20:01:36 +0200

    Try new features in IntelliJ experimental

--- src/test/java/net/vince/gists/Test.java
+++ src/test/java/net/vince/gists/Test.java
@@ -1,5 +1,39 @@
 package net.vince.gists;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+
 public class Test {
 
+  @org.junit.jupiter.api.Test
+  @DisplayName("Switch expression")
+  public void testSwitch() {
+    final var intValue = 1;
+    final var textValue =
+        switch (intValue) {
+          case 1:
+            break "one";
+          case 2:
+            break "two";
+          case 3:
+            break "three";
+          default:
+            break "don't know";
+        };
+
+    assertEquals("one", textValue);
+  }
+
+  @org.junit.jupiter.api.Test
+  @DisplayName("String literals")
+  public void testLiteral() {
+    final var literal =
+        `
+          String literals
+        `;
+
+    assertTrue(literal.contains("\n"));
+  }
 }
